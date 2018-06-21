06eb898a24d36d471ac864c9452250b1b7e8aaa9
GraceAHerr <mazyg321@gmail.com>
2018-06-21 17:59:57 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,22 +1,30 @@
 #code your solutions here
 perfect_10_recipe = {
-  :almond_flour => 2 cups
-  :gluten_freewhole_oats =>1 cup
-  :baking_powder  => 1/2 tea spoon
-  :baking_soda =>1/4 tea spoon
-  :Xanthan_Gum =>1/2 tea spoon
-  :slivered_almonds_lightly toasted => 1/4 cup
-  :mini_dark_chocolate_chips => 3/4 cup
-  :olive_oil => 1/2 cup
-  :agave => 1/4 cup
+  :almond_flour => "2 cups"
+  :gluten_freewhole_oats => "1 cup"
+  :baking_powder  => "1/2 tea spoon"
+  :baking_soda => "1/4 tea spoon"
+  :Xanthan_Gum => "1/2 tea spoon"
+  :slivered_almonds_lightly_toasted => "1/4 cup"
+  :mini_dark_chocolate_chips => "3/4 cup"
+  :olive_oil => "1/2 cup"
+  :agave => "1/4 cup"
 }
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 
-# All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
+
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
-end
+  return :almond_flour => "2 cups"
+  return :gluten_freewhole_oats => "1 cup"
+  return :baking_powder  => "1/2 tea spoon"
+  return :baking_soda => "1/4 tea spoon"
+  return :Xanthan_Gum => "1/2 tea spoon"
+  return :slivered_almonds_lightly_toasted => "1/4 cup"
+  return :mini_dark_chocolate_chips => "3/4 cup"
+  return :olive_oil => "1/2 cup"
+  return :agave => "1/4 cup"
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
